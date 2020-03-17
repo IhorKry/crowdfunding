d8ae57ab1e7db392bd9b432ddd2aa0f62331085e
Igor Kryvopustov <ihorkr@tahometer.com>
2020-03-17 22:53:15 +0200

    Create aim controller

---
+++ Controllers/AimController.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using crowdfunding.Models;
+using crowdfunding.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace crowdfunding.Controllers
+{
+    [ApiController]
+    [Route("api/aims")]
+    public class AimController : ControllerBase
+    {
+        private AimService Service;
+
+        public AimController(AimService service)
+        {
+            Service = service;
+        }
+
+        // POST api/aims
+        [HttpPost]
+        public ActionResult Create(Aim aim)
+        {
+            return Ok(Service.CreateAim(aim));
+        }
+
+        // TODO: Think about get all aims possibility 
+        // GET api/aims
+        [HttpGet]
+        public List<Aim> GetAims()
+        {
+            return Service.GetAims();
+        }
+
+        // GET api/aims/2
+        [HttpGet("{id}")]
+        public ActionResult<Aim> GetById(int id)
+        {
+            var result = Service.GetAimById(id);
+
+            if (result == null) return NotFound();
+
+            return Ok(result);
+        }
+
+        // TODO: Think about delete possibility 
+        // DELETE api/aims/2
+        [HttpDelete("{id}")]
+        public ActionResult DeleteById(int id)
+        {
+            var result = Service.GetAimById(id);
+
+            if (result == null) return NotFound();
+
+            Service.DeleteAimById(result);
+            return NoContent();
+        }
+    }
+}
