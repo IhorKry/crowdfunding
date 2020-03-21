7deab06a57865567c2ad5b9d0e6c6ea12704d575
Igor Kryvopustov <ihorkr@tahometer.com>
2020-03-21 17:03:11 +0200

    Create founder controller and service with minimum functional

--- Startup.cs
+++ Startup.cs
@@ -37,6 +37,7 @@ namespace crowdfunding
             });
 
             services.AddScoped<AimService, AimService>();
+            services.AddScoped<FounderService, FounderService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
